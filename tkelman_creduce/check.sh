2a02f4d5fd31a20cee6d8aaf8e85fe708bc0ed90
Julia Buildbot <buildbot@julialang.org>
2017-03-16 17:54:44 +0000

    Better parameterization of check.sh

--- tkelman_creduce/check.sh
+++ tkelman_creduce/check.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+BASE_DIR="/src/mwe/tkelman_creduce"
 LDFLAGS="-lm -lpthread -lgomp"
 CPPFLAGS="-g -I/src/julia/deps/scratch/SuiteSparse-4.4.5/UMFPACK/Include/ -I/src/julia/deps/scratch/SuiteSparse-4.4.5/UMFPACK/Source -I/src/julia/deps/scratch/SuiteSparse-4.4.5/AMD/Include/ -I/src/julia/deps/scratch/SuiteSparse-4.4.5/SuiteSparse_config/ -DZLONG"
-MWE_C="/src/mwe/tkelman_creduce/mwe.c"
 
 # Cleanup from previous attempts, and ensure we don't dump core
 ulimit -S -c 0
 rm -f ./mwe_O0 ./mwe_O2 ./mwe_LU.dat
 
 # First, compile working
-gcc -O0 ${CPPFLAGS} -DLONGBLAS="long long" ${MWE_C} *.i /src/mwe/*.a ${LDFLAGS} -o mwe_O0
+gcc -O0 ${CPPFLAGS} -DLONGBLAS="long long" ${BASE_DIR}/mwe.c *.i /src/mwe/*.a ${LDFLAGS} -o mwe_O0
 
 if [[ "$?" != "0" ]]; then
 	echo "This doesn't even compile with -O0!"
@@ -24,7 +24,7 @@ if [[ "$?" != "0" ]]; then
 fi
 
 # Ensure that the factorized output is the same
-if ! cmp mwe_LU.dat known_good.dat; then
+if ! cmp mwe_LU.dat ${BASE_DIR}/known_good.dat; then
 	echo "The files did not match!"
 	exit 1
 fi
@@ -32,7 +32,7 @@ fi
 echo "mwe_O0 passed all tests"
 
 # Next, compile broken
-gcc -O2 -ftree-slp-vectorize ${CPPFLAGS} -DLONGBLAS="long long" ${MWE_C} *.i /src/mwe/*.a ${LDFLAGS} -o mwe_O2
+gcc -O2 -ftree-slp-vectorize ${CPPFLAGS} -DLONGBLAS="long long" ${BASE_DIR}/mwe.c *.i /src/mwe/*.a ${LDFLAGS} -o mwe_O2
 
 if [[ "$?" != "0" ]]; then
 	echo "This doesn't even compile with -O2!"
